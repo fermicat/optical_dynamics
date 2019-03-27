3ab7e0b0af7ecc63b7da50cedef3df287909f57e
quantumcatwang <40706814+quantumcatwang@users.noreply.github.com>
2019-03-27 13:51:44 -0500

    Add files via upload

--- main.c
+++ main.c
@@ -4,49 +4,51 @@
 #include <complex.h>
 #include "physics_constants.h"
 
-/// @dev change ii -> I
-/// @dev complex<double> -> double_Complex
-/// @dev slope
-/// @dev debug print
 
 /************************************** parameters ******************************************/
     //cavity parameters:
-    double L  = 1e-3, rl = 1, rr = 0.60;                      // L = 3mm, Rl > 95%, Rr^2 = 36%
-    double dz = 1e-6;                   // dt >= 1e-13: integration divergence
-    int    Nz = (int) (L/dz);
+    double L  = 3e-3, rl = 1, rr = 0.60;                      // L = 3mm, Rl > 95%, Rr^2 = 36%
+    double dz = 1e-6;                   
+    int    Nz;
     double h  = 3e-4, J  = 0.8;
 
     // laser parameter
-    double T1      = 1e-12,     T2     = 50e-15,    Diff = 1;          // T1 = 1ns, T2 = 50fs, Diff = 46cm2s^-1
+    double T2     = 50e-15,    Diff = 0.0046;          // T1 = 1ns, T2 = 50fs, Diff = 46cm2s^-1
     double overlap = 0.1,       dipole = 0.8e-9 * echarge;
-    double nr      = 3.5,       loss    = 500.0;
-    double rho_e   = 5.62e22;                                   // for testing 
+    double nr      = 3.5,       loss   = 500.0;
+    double rho_e   = 4.34e22;                                   // for testing 
     double lambda0 = 3.2e-6;
-    double Etransition = hbar * c0 * 2.0 * PI / lambda0; 
-    double kwave       = nr * Etransition / (c0 * hbar);
-    double G = overlap * dipole * (Etransition/hbar) * rho_e / (nr * epsilon0 * c0);
+    double kwave, G;
 
     // time parameter
-    int    Nt = 500;    
-    double dt = 1e-14;
+    int    Nt;    
+    double dt = 1e-14;              // dt > 1e-13 integral divergence
 
 /************************************ laser state ********************************************/
     double *n0;
     double _Complex *Ep, *En, *n2, *etap, *etan, *dzEp, *dzEn;
     // slope
+    double **k0;
+    double _Complex **k2, **kap, **kan, **kp, **kn;
+
+    //copy
     double *n0_old;
-    complex<double> *Ep_old, *En_old, *n2_old, *etap_old, *etan_old;
-
-    // copy
-
+    double _Complex *Ep_old, *En_old, *n2_old, *etap_old, *etan_old, *dzEp_old, *dzEn_old;
+    
+    double _Complex *Eout;
 
 /****************************************** PDE **********************************************/
  //Neq is equi-population-inversion
     //z = iz*dz, 0<z<h is gain region
     double Neq(double z) {
-        return 2 * J - 1;
-        //if (z < h) return 0.6;
-        //else return 0.1; 
+        //return 2 * J - 1;
+        if (z < L - h) return 0.6;
+        else return 0.2; 
+    }
+
+    double T1(double z) {
+        if (z < L - h) return 1e-12;
+        else return 1e-13;
     }
 
     /**
@@ -56,50 +58,50 @@
     void dt_n0(int round) {
         for (int j = 0; j < Nz; j++) {
             double z = j * dz;
-            double temp = (Ep[j] * conj(etap[j]) + En[j] * conj(etan[j])).imag();
-            k0[round][j] = - (n0[j] - Neq(z)) / T1 - 2.0 * dipole / hbar * temp;
+            double temp = cimag( (Ep[j] * conj(etap[j]) + En[j] * conj(etan[j])) );
+            k0[round][j] = - (n0[j] - Neq(z)) / T1(z) - 2.0 * dipole / hbar * temp;
         } 
     }
 
     void dt_n2(int round) {
         for (int j = 0; j < Nz; j++) {
-            complex<double> temp = Ep[j] * conj(etan[j]) - conj(En[j]) * etap[j];
-            k2[round][j] = - (1 / T1 + 4 * kwave * kwave * Diff) * n2[j] + ii * dipole / hbar * temp;
+            complex temp = Ep[j] * conj(etan[j]) - conj(En[j]) * etap[j];
+            k2[round][j] = - (1 / T1(j * dz) + 4 * kwave * kwave * Diff) * n2[j] + I * dipole / hbar * temp;
         }
     }
 
     void dt_etap(int round) {
         for (int j = 0; j < Nz; j++) {
-            complex<double> temp = n0[j] * Ep[j] + n2[j] * En[j];
-            kap[round][j] = - ii * dipole / (2*hbar) * temp - etap[j] / T2;
+            complex temp = n0[j] * Ep[j] + n2[j] * En[j];
+            kap[round][j] = - I * dipole / (2*hbar) * temp - etap[j] / T2;
         }
     }
 
     void dt_etan(int round) {
         for (int j = 0; j < Nz; j++) {
-            complex<double> temp = n0[j] * En[j] + conj(n2[j]) * Ep[j];
-            kan[round][j] - ii * dipole / (2*hbar) * temp - etan[j] / T2;
+            complex temp = n0[j] * En[j] + conj(n2[j]) * Ep[j];
+            kan[round][j] = - I * dipole / (2*hbar) * temp - etan[j] / T2;
         }
     }
   
     void dt_Ep(int round) {
         for (int j = 0; j < Nz; j++) {
-            kp[round][j] = (c0/nr) * (ii * G * etap[j] - loss * Ep[j] - dzEp[j]);
+            kp[round][j] = (c0/nr) * (I * G * etap[j] - loss * Ep[j] - dzEp[j]);
         }
     }
 
     void dt_En(int round) {
         for (int j = 0; j < Nz; j++) {
-            kn[round][j] = (c0/nr) * (ii * G * etan[j] - loss * En[j] + dzEn[j]);
+            kn[round][j] = (c0/nr) * (I * G * etan[j] - loss * En[j] + dzEn[j]);
         }
     }
     
     // dE/dz 
-    void gradE(complex<double> dzE[], complex<double> E[]) {
+    void gradE(complex *dzE, complex *E) {
         dzE[0] = (E[1] - E[0]) / dz;
-        dzE[Nz] = (E[Nz] - E[Nz-1]) / dz;
+        dzE[Nz - 1] = (E[Nz - 1] - E[Nz - 2]) / dz;
         for (int j = 1; j < Nz - 1; j++) {
-            dzE[j] = (E[j + 1] - E[j - 1]) / (2*dz);
+            dzE[j] = (E[j + 1] - E[j - 1]) / (2 * dz);
         }
     }
 
@@ -170,27 +172,27 @@
 /********************************** State Initialization *********************************/
 
     // initialize the E(z, t=0)
-    void field_initialization(complex<double> Epl, complex<double> Epr) {
-        complex<double> Enl, Enr;
+    void field_initialization(complex Epl, complex Epr) {
+        complex Enl, Enr;
         Enr = Epr * rr;                                     //reflection
         Enl = Epl / rl;
-        complex<double> kEp = (Epr - Epl) / double(Nz - 1);
-        complex<double> kEn = (Enr - Enl) / double(Nz - 1);
+        complex kEp = (Epr - Epl) / (Nz - 1.0);
+        complex kEn = (Enr - Enl) / (Nz - 1.0);
         for (int i = 0; i < Nz; i++) {
-            Ep[i] = Epl + double(i) * kEp;
-            En[i] = Enl + double(i) * kEn;
+            Ep[i] = Epl + (double)(i) * kEp;
+            En[i] = Enl + (double)(i) * kEn;
         }
     }
 
     //initialize the n(z, t=0)
-    void population_initialization(double _n0, complex<double> _n2) {
+    void population_initialization(double _n0, complex _n2) {
         for (int i = 0; i < Nz; i++) {
             n0[i] = _n0;       
             n2[i] = _n2;
         }
     }
 
-    void eta_initialization(complex<double> _etap, complex<double> _etan) {
+    void eta_initialization(complex _etap, complex _etan) {
         for (int i = 0; i < Nz; i++) {
             etap[i] = _etap;
             etan[i] = _etan;
@@ -209,9 +211,11 @@
     }
 
     void model() {
+        void showSteps(int t);
+
         for (int i = 0; i < Nt; i++) {
             copyArray();
-            Eout[i] = (1 - rr) * Ep[Nz];                      //output field
+            Eout[i] = (1 - rr) * Ep[Nz - 1];                      //output field
 
             // Runge-Kutta
             for (int round = 1; round <= 4; round++) {
@@ -222,62 +226,143 @@
             }
             
             Ep[0] = rl * En[0];                                 
-            En[Nz] = rr * Ep[Nz];
+            En[Nz - 1] = rr * Ep[Nz - 1];
         
-            //debugPrint(i);
+            showSteps(i);
         }
     }
 
+/**************************** system & malloc & free ****************************/
+    void showSteps(int t) {
+        if (t % 500 == 0) {
+            printf("t = %d\n", t);
+            printf("Eout = %f, \tn0 = %f, \tn2 = %f\n", creal(Eout[t]), n0[Nz - 1], creal(n2[Nz - 1]));
+        }
+    }
+
+    void creatArray() {
+        // new for laser state
+        n0   = (double*) malloc(sizeof(double) * Nz);
+        n2   = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
+        etap = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
+        etan = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
+        Ep   = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
+        En   = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
+        dzEp = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
+        dzEn = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
+
+        // new for copy state
+        n0_old   = (double*) malloc(sizeof(double) * Nz);
+        n2_old   = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
+        etap_old = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
+        etan_old = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
+        Ep_old   = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
+        En_old   = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
+        dzEp_old = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
+        dzEn_old = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
+
+        Eout     = (complex*) malloc(sizeof(complex) * Nt);
+
+        // new for slope
+        k0  = (double**)  malloc(sizeof(double*) * 5);
+        k2  = (complex**) malloc(sizeof(complex*) * 5);
+        kap = (complex**) malloc(sizeof(complex*) * 5);
+        kan = (complex**) malloc(sizeof(complex*) * 5);
+        kp  = (complex**) malloc(sizeof(complex*) * 5);
+        kn  = (complex**) malloc(sizeof(complex*) * 5);
+
+        for (int i = 0; i < 5; i++) {
+            k0[i]  = (double*)  malloc(sizeof(double) * Nz);
+            k2[i]  = (complex*) malloc(sizeof(complex) * Nz);
+            kap[i] = (complex*) malloc(sizeof(complex) * Nz);
+            kan[i] = (complex*) malloc(sizeof(complex) * Nz);
+            kp[i]  = (complex*) malloc(sizeof(complex) * Nz);
+            kn[i]  = (complex*) malloc(sizeof(complex) * Nz);
+        }
+
+    }
+
+    void deleteArray() {
+        free(n0);
+        free(n2);
+        free(etap);
+        free(etan);
+        free(Ep);
+        free(En);
+        //free(dzEp);
+        free(dzEn);
+        
+        free(n0_old);
+        free(n2_old);
+        free(etap_old);
+        free(etan_old);
+        
+        free(Ep_old);
+        free(En_old);
+        //free(dzEp_old);
+        free(dzEn_old);
+        
+        free(Eout);
+        
+        for (int i = 0; i < 5; i++) {
+            free(k0[i]);
+            free(k2[i]);
+            free(kap[i]);
+            free(kan[i]);
+            free(kp[i]);
+            free(kn[i]);
+        }
+        free(k0);
+        free(k2);
+        free(kap);
+        free(kan);
+        free(kp);
+        free(kn);
+    }
 
 
 /**************************************** Main ********************************/
 
 int main() {
-    // new for laser state
-    n0   = (double*) malloc(sizeof(double) * Nz);
-    n2   = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
-    etap = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
-    etan = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
-    Ep   = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
-    En   = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
-    dzEp = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
-    dzEn = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
+    // quantity
+    double Etransition = hbar * c0 * 2.0 * PI / lambda0; 
+    kwave = nr * Etransition / (c0 * hbar);
+    G     = overlap * dipole * (Etransition/hbar) * rho_e / (nr * epsilon0 * c0);
+    Nz    = (int) (L / dz);
+    Nt    = 500000;
 
-    // new for copy state
-    n0_old   = (double*) malloc(sizeof(double) * Nz);
-    n2_old   = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
-    etap_old = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
-    etan_old = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
-    Ep_old   = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
-    En_old   = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
-    dzEp_old = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
-    dzEn_old = (double _Complex*) malloc(sizeof(double _Complex) * Nz);
-
-    Eout = new complex<double>[Nt];
-
-    // new for slope
-
-
+    // malloc resource
+    creatArray();
+    
     // initialization
-    complex<double> Epl = 5e2 + ii;
-    complex<double> Epr = 2e2 + 2.0 * ii;
+    complex Epl = 5e4 + I, Epr = 2e4 + 2.0 * I;
     field_initialization(Epl, Epr);
     
-    double _n0 = 0.56;  
-    complex<double> _n2 = 0.0;
+    double  _n0 = 0.56;  
+    complex _n2 = 0.0;
     population_initialization(_n0, _n2);
     
-    complex<double> _etap = 0.001, _etan = 0.001;
+    complex _etap = 0.001, _etan = 0.001;
     eta_initialization(_etap, _etan);
 
     // dynamics
     model();
 
+    // file
     FILE *fp = NULL;
     fp = fopen("./data.dat", "w+");
+    
+    fprintf(fp, "#time(ps)\t Re[E]\t Im[E] \t |E| \t |E|^2 \n");
+    for(int j = 0; j <= Nt; j++)
+    {
+        fprintf(fp, "%f\t%f\t%f\t", j*dt/1e-12, creal(Eout[j]), cimag(Eout[j]));
+        double intensity = (double) (conj(Eout[j]) * Eout[j]);
+        fprintf(fp, "%f\t%f\n", sqrt(intensity), intensity );
+    }
     
     fclose(fp);
 
     //free
-
+    deleteArray();
+    return 0;
 }
